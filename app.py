0ce94269f1edcd1f39db1d36eb3e87fee10e2175
Mahesh C. Regmi <geekyarthurs@gmail.com>
2019-11-08 13:50:49 +0545

    gunicorn

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, request, redirect, url_for, render_template, send_file,
 from werkzeug.utils import secure_filename
 from face_detection import face_detector
 UPLOAD_FOLDER = '/uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -13,9 +13,15 @@ def upload_image():
     if request.method == 'POST':
         print(request.files)
         if 'file' not in request.files:
+        
+
             
             return 'Image Not Found'
+        
+        
         file = request.files['file']
+        if not allowed_image(file):
+            return "Invalid Image :D Not hacking"
         secure_file = file.filename
         file.save("./uploads/" + secure_file)
         face_detector("./uploads/" + secure_file)
@@ -36,6 +42,22 @@ def upload_image():
     if request.method == 'GET':
         
         return render_template("index.html")
+
+def allowed_image(filename):
+    
+    # We only want files with a . in the filename
+    if not "." in filename:
+        return False
+
+    # Split the extension from the filename
+    ext = filename.rsplit(".", 1)[1]
+
+    # Check if the extension is in ALLOWED_IMAGE_EXTENSIONS
+    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
+        return True
+    else:
+        return False
+
 
 if __name__ == '__main__':
     app.run()
